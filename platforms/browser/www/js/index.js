6f79c2b5056173c3c4d3e20aa649799c47f47f66
ritika <ritika-futurework>
2018-03-15 15:21:55 +0530

    project added by shashi

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,24 @@
+var app = {
+  // Application Constructor
+  initialize: function() {
+      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+      
+	
+	   
+
+        // onError Callback receives a PositionError object
+        //
+       
+
+        
+		
+};
+
+app.initialize();
